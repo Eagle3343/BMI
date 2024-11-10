b3ecf60b1df01272ec56e5f06ca6e35da80ec7db
Eagle3343 <Eagle3343>
2024-11-10 20:17:25 +0400

    code solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,20 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+function BMICalculator(weight,height){
+
+    return  (weight / ((height / 100) ** 2));
+    }
+    
+    
+    let aliBMI= BMICalculator(70,180);
+    console.log(aliBMI);
+    
+    let mohamedBMI =BMICalculator(90,174);
+    console.log(mohamedBMI);
+    
+    
+    console.log("mohamed's BMI is " + mohamedBMI.toFixed(0) + " which is greater than ali "+aliBMI.toFixed(0));
+    
+      
